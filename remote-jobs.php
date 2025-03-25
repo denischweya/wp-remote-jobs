432f4d7622cad20cb049946cc8165ec6c8298c0e
denischweya <denisc@easternstandard.com>
2025-03-26 01:52:48 +0300

    v1.0.2

--- remote-jobs.php
+++ remote-jobs.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Remote Jobs
  * Plugin URI:        https://denis.swishfolio.com/
  * Description:       A simple job board plugin for WordPress
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 5.8
  * Requires PHP:      7.4
  * Author:            Afrothemes
